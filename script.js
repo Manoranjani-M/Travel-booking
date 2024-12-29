b488bc42fb157e3b64891b276f791197893e6ca0
Manoranjani M <161202128+Manoranjani-M@users.noreply.github.com>
2024-12-29 16:31:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+let home2=document.getElementById("home")
+let service2=document.getElementById("service")
+let pricing2=document.getElementById("pricing")
+let btn=document.getElementById("btn")
+let contactUs2=document.getElementById("contactUs")
+let submit2=document.getElementById("submit");
+
+function home(){
+     
+    home2.style.cssText += "display:block; transition: .5s";
+    service2.style.display="none";
+    pricing2.style.display="none";
+    contactUs2.style.display="none";
+
+}
+function service(){
+    home2.style.display="none";
+    service2.style.cssText += "display:block; transition: 2s;";
+    pricing2.style.display="none";
+    contactUs2.style.display="none";
+}
+function pricing(){
+    home2.style.display="none";
+    service2.style.display="none";
+    pricing2.style.display="block";
+    contactUs2.style.display="none";
+}
+
+function contactUs(){
+    btn.textContent="Sing up"
+    home2.style.display="none";
+    service2.style.display="none";
+    pricing2.style.display="none";
+    contactUs2.style.display="block";
+}
+
+function submit(){
+    alert("Message sended");
+}
+
+submit2.addEventListner("click",function(){
+    alert("jhgksg")
+});
